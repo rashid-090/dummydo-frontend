2d1e0de886b093d931003bd60e8fc1fe4258ebd0
Rashid <rashidpk090@gmail.com>
2024-04-05 15:21:32 +0530

    cahnges

--- src/components/Searchs/index.tsx
+++ src/components/Searchs/index.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { MdFlight } from "react-icons/md";
 import { FaHotel } from "react-icons/fa6";
 import { LuArrowRightLeft } from "react-icons/lu";
 import Select from 'react-select';
 import Checkbox from '@mui/material/Checkbox';
 import { IoArrowForwardSharp } from "react-icons/io5";
-
-
-
+import Switch from "react-switch";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { DateRange } from 'react-date-range';
+import { format } from "date-fns";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
 const options = [
     { value: 'Calicut', label: 'Calicut' },
     { value: 'Dubai', label: 'Dubai' },
@@ -21,6 +24,56 @@ const SearchPage = () => {
   
     const [selectedOption, setSelectedOption] = useState(null);
     const [activeTab, setActiveTab] = useState('flight');
+    const [checked, setChecked] = useState(false);
+    const [passengerCount, setPassengerCount] = useState(0);
+    const [openDate, setOpenDate] = useState(false);
+    const modalRef = useRef<HTMLDivElement>(null); 
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
+
+
+      useEffect(() => {
+        // Function to handle click outside of modal
+        function handleClickOutside(event: MouseEvent) {
+          if (modalRef.current && !modalRef.current.contains(event.target as Node)) { // Type assertion to Node
+            setOpenDate(false);
+          }
+        }
+    
+        // Add event listener when modal is open
+        if (openDate) {
+          document.addEventListener('mousedown', handleClickOutside);
+        } else {
+          // Remove event listener when modal is closed
+          document.removeEventListener('mousedown', handleClickOutside);
+        }
+    
+        // Cleanup function to remove event listener
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, [openDate]);
+
+    const increasePassengerCount = () => {
+        if (passengerCount < 10) {
+          setPassengerCount(prevCount => prevCount + 1);
+        }
+      };
+    
+      const decreasePassengerCount = () => {
+        if (passengerCount > 0) {
+          setPassengerCount(prevCount => prevCount - 1);
+        }
+      };
+
+    const handleCheck = nextChecked => {
+      setChecked(nextChecked);
+    };
 
     const handleTabChange = (tabId: any) => {
         setActiveTab(tabId);
@@ -45,6 +98,7 @@ const SearchPage = () => {
     return (
         <div className="w-full bg-white rounded-2xl xl:rounded-[3rem] shades p-5 lg:p-10">
             <div className="flex mb-4 text-base font-medium">
+                
                 <button
                     className={`px-4 py-2 flex items-center gap-2 rounded-full ${activeTab === 'flight' ? 'bg-main text-white' : 'bg-gray-100 text-gray-800'
                         }`}
@@ -62,22 +116,48 @@ const SearchPage = () => {
             </div>
             {/* Flights */}
             {activeTab === 'flight' && (
-                <form className='w-full'>
-                    <div className='flex gap-3 py-3'>
-                       <span className='flex items-center'>
-                            <Checkbox defaultChecked sx={{color:'#f0582b','&.Mui-checked': {color: '#f0582b',}}} required/>
-                            <label className='text-sm capitalize font-medium'>Ony way</label>
-                       </span>
-                       <span className='flex items-center'>
-                            <Checkbox  sx={{color:'#f0582b','&.Mui-checked': {color: '#f0582b',}}} required/>
-                            <label className='text-sm capitalize font-medium'>round trip</label>
-                       </span>
+                <>
+                   <div className='flex gap-10'>
+                     {/* toolge */}
+                
+                     <div className='flex gap-2 items-center h-14 capitalize text-sm font-medium'>
+                                <p>one way</p>
+                                <Switch
+                                onChange={handleCheck}
+                                checked={checked}
+                                onColor="#fddfd6"
+                                onHandleColor="#e4542a"
+                                offColor="#fddfd6"
+                                offHandleColor="#e4542a"
+                                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                                height={20}
+                                width={48}
+                                className="react-switch"
+                                checkedIcon={false}
+                                uncheckedIcon={false}
+                                    />
+                                <p>Round trip</p>
+                            </div>
+                     {/* toolge */}
+                     {/* Passenger */}
 
-                    </div>
+                            <div className="flex items-center gap-2 selection:bg-none">
+                                <span className="text-black font-medium px-1 text-center">Adults:</span>
+                                <AiFillMinusCircle className={`text-main text-xl  ${passengerCount <= 0 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`} onClick={decreasePassengerCount}/>
+                                    <span className="text-gray-600 font-medium  text-center">{passengerCount}</span>
+                                <AiFillPlusCircle className={`text-main text-xl cursor-pointer ${passengerCount >= 10 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`} onClick={increasePassengerCount}/>
+                                 
+                            </div>
+                    
+                     {/* Passenger */}
+                   </div>
+                    <form className='w-full'>
+                    
                    <div className='flex flex-col md:flex-row items-center gap-4'>
                     <Select
                             placeholder='From'
-                            className='w-full'
+                            className='w-full font-medium'
                             styles={style}
                             defaultValue={selectedOption}
                             onChange={handleChange}
@@ -86,54 +166,115 @@ const SearchPage = () => {
                     <LuArrowRightLeft className='text-main rotate-90 md:rotate-0 text-xl w-20'/>
                     <Select
                             placeholder='To'
-                            className='w-full'
+                            className='w-full font-medium'
                             styles={style}
                             defaultValue={selectedOption}
                             onChange={handleChange}
                             options={options}
+                            /> 
+                    <div className='relative w-full' ref={modalRef}>
+                        <span  className='border border-main font-medium py-2 text-gray-700 text-sm  rounded-md w-full grid place-items-center' onClick={() => setOpenDate(!openDate)}>
+                                {`${format(date[0].startDate, "MMM d yyyy")} - ${format(
+                                date[0].endDate,
+                                "MMM d yyyy"
+                                )}`}
+                        </span>
+                        {openDate && (
+                            <DateRange
+                                editableDateInputs={true}
+                                onChange={(item) => setDate([item.selection])}
+                                moveRangeOnFirstSelection={false}
+                                ranges={date}
+                                className="customdate"
+                                minDate={new Date()}
                             />
-                    <input className='outline-none h-9 w-full px-5 rounded-md border-main border' type="date" name="" id="" />
-                    <input className='outline-none h-9 w-full px-5 rounded-md border-main border' type="date" name="" id="" />
-                    <button className='text-2xl text-white bg-main border border-main w-full lg:w-80 h-10 md:h-9 grid place-items-center rounded-full' type='submit'><IoArrowForwardSharp/></button>
+                            )}
+                    </div>
+                    <button className='bg-main w-full md:w-32 rounded-full h-9 grid place-items-center text-white'><IoArrowForwardSharp/></button>
                    </div>
                 </form>
+                </>
             )}
               {/* Hotels */}
             {activeTab === 'fighthotel' && (
-                 <form className='w-full'>
-                 <div className='flex gap-3 py-3'>
-                    <span className='flex items-center'>
-                         <Checkbox defaultChecked sx={{color:'#f0582b','&.Mui-checked': {color: '#f0582b',}}} required/>
-                         <label className='text-sm capitalize font-medium'>Ony way</label>
-                    </span>
-                    <span className='flex items-center'>
-                         <Checkbox  sx={{color:'#f0582b','&.Mui-checked': {color: '#f0582b',}}} required/>
-                         <label className='text-sm capitalize font-medium'>round trip</label>
-                    </span>
+                  <>
+                  <div className='flex gap-10'>
+                    {/* toolge */}
+               
+                    <div className='flex gap-2 items-center h-14 capitalize text-sm font-medium'>
+                               <p>one way</p>
+                               <Switch
+                               onChange={handleCheck}
+                               checked={checked}
+                               onColor="#fddfd6"
+                               onHandleColor="#e4542a"
+                               offColor="#fddfd6"
+                               offHandleColor="#e4542a"
+                               boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                               activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                               height={20}
+                               width={48}
+                               className="react-switch"
+                               checkedIcon={false}
+                               uncheckedIcon={false}
+                                   />
+                               <p>Round trip</p>
+                           </div>
+                    {/* toolge */}
+                    {/* Passenger */}
 
-                 </div>
-                <div className='flex flex-col md:flex-row items-center gap-4'>
-                 <Select
-                         placeholder='From'
-                         className='w-full'
-                         styles={style}
-                         defaultValue={selectedOption}
-                         onChange={handleChange}
-                         options={options}
-                         />
-                 <LuArrowRightLeft className='text-main rotate-90 md:rotate-0 text-xl w-20'/>
-                 <Select
-                         placeholder='To'
-                         className='w-full'
-                         styles={style}
-                         defaultValue={selectedOption}
-                         onChange={handleChange}
-                         options={options}
-                         />
-                 <input className='outline-none h-9 w-full px-5 rounded-md border-main border' type="date" name="" id="" />
-                 <button className='text-2xl text-white bg-main border border-main w-full lg:w-80 h-10 md:h-9 grid place-items-center rounded-full' type='submit'><IoArrowForwardSharp/></button>
-                </div>
-             </form>
+                           <div className="flex items-center gap-2 selection:bg-none">
+                               <span className="text-black font-medium px-1 text-center">Adults:</span>
+                               <AiFillMinusCircle className={`text-main text-xl  ${passengerCount <= 0 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`} onClick={decreasePassengerCount}/>
+                                   <span className="text-gray-600 font-medium  text-center">{passengerCount}</span>
+                               <AiFillPlusCircle className={`text-main text-xl cursor-pointer ${passengerCount >= 10 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`} onClick={increasePassengerCount}/>
+                                
+                           </div>
+                   
+                    {/* Passenger */}
+                  </div>
+                   <form className='w-full'>
+                   
+                  <div className='flex flex-col md:flex-row items-center gap-4'>
+                   <Select
+                           placeholder='From'
+                           className='w-full font-medium'
+                           styles={style}
+                           defaultValue={selectedOption}
+                           onChange={handleChange}
+                           options={options}
+                           />
+                   <LuArrowRightLeft className='text-main rotate-90 md:rotate-0 text-xl w-20'/>
+                   <Select
+                           placeholder='To'
+                           className='w-full font-medium'
+                           styles={style}
+                           defaultValue={selectedOption}
+                           onChange={handleChange}
+                           options={options}
+                           /> 
+                   <div className='relative w-full' ref={modalRef}>
+                       <span  className='border border-main font-medium py-2 text-gray-700 text-sm  rounded-md w-full grid place-items-center' onClick={() => setOpenDate(!openDate)}>
+                               {`${format(date[0].startDate, "MMM d yyyy")} - ${format(
+                               date[0].endDate,
+                               "MMM d yyyy"
+                               )}`}
+                       </span>
+                       {openDate && (
+                           <DateRange
+                               editableDateInputs={true}
+                               onChange={(item) => setDate([item.selection])}
+                               moveRangeOnFirstSelection={false}
+                               ranges={date}
+                               className="customdate"
+                               minDate={new Date()}
+                           />
+                           )}
+                   </div>
+                   <button className='bg-main w-full md:w-32 rounded-full h-9 grid place-items-center text-white'><IoArrowForwardSharp/></button>
+                  </div>
+               </form>
+               </>
             )}
       
           
--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -3,12 +3,12 @@ import {vid1,vid2} from '../../assets'
 
 const Banner = () => {
   return (
-    <main className='grid grid-cols-1 md:grid-cols-2 gap-0 xl:gap-5 pt-10 xl:pt-16'>
+    <main className='grid grid-cols-1 md:grid-cols-2 gap-0  xl:gap-5 pt-5 xl:pt-0'>
     <div className='flex flex-col gap-y-2 justify-center'>
         <h1 className='text-2xl xl:text-4xl font-bold text-main'>Lorem ipsum dolor.</h1>
         <p className='text-gray-500 text-sm xl:text-base font-medium'>Lorem ipsum dolor sit amet consectetur,<br/> adipisicing elit. Consequatur, iure.</p>
     </div>
-     <div className=" relative w-full grid place-items-end overflow-hidden z-10">
+     <div className=" relative w-full  grid place-items-end overflow-hidden z-10">
             <video
                 className="aspect-video"
                 autoPlay
