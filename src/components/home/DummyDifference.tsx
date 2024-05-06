2449e16987f41b8b3ff5c5299f4bf0286776697c
Rashid <rashidpk090@gmail.com>
2024-05-06 17:27:00 +0530

    asd

--- src/components/home/DummyDifference.tsx
+++ src/components/home/DummyDifference.tsx
@@ -26,9 +26,8 @@ const Diffdata = [
 const DummyDifference = () => {
   return (
     <>
-     <div className='xl:pl-44 pt-20  xl:py-20 relative'>
-          <div className='block mb-10 md:hidden xl:block ml-10 md:ml-0 h-52 xl:h-full bg-gradient-to-r from-bg-from to-bg-to rounded-l-[15rem]'>
-          </div>
+     <div className='xl:pl-44   xl:py-20 relative'>
+        
         <div className='px-5 md:px-8 xl:px-0 grid grid-cols-1 md:grid-cols-3 '>
           <h1 className='text-2xl lg:text-4xl xl:text-7xl font-bold text-center  xl:text-left text-black font-futuracondensed uppercase '>choose <br className='hidden xl:block'/><span className='gradient-text'>DummyFree.</span></h1>
           <div className='col-span-2'>
@@ -44,6 +43,8 @@ const DummyDifference = () => {
                   <p className='text-xs sm:text-sm text-gray-500 font-medium'>{dt.content}</p>
                 </div>
               ))}
+          </div>
+          <div className='block mb-10 md:hidden xl:block ml-10 md:ml-0 h-52 xl:h-full bg-gradient-to-r from-bg-from to-bg-to rounded-l-[15rem]'>
           </div>
           <div className='hidden xl:block ml-5 md:ml-0 h-60 xl:h-full bg-gradient-to-r from-bg-from to-bg-to rounded-l-[15rem]'>
 
