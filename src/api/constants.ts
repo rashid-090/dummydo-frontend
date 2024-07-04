03513eb1aa806a65a0f0abed70df2b76f51a1521
Rashid <rashidpk090@gmail.com>
2024-07-04 13:17:33 +0530

    asd

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://dummydo.com/api"
-export const BASE_URL = "https://workplacecodedone.online/api"
+export const BASE_URL = "https://dummydo-backend-production.up.railway.app/api"
 // export const BASE_URL = "http://localhost:2024/api"
