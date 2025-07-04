d7dac1a8563f694c240352063ff2fb4b7cd2295e
Rashid <rashidpk090@gmail.com>
2025-07-04 21:05:53 +0530

    done

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,4 @@
 // export const BASE_URL = "https://dummydo.com/api"
-export const BASE_URL = "https://dummy-backend-final-production.up.railway.app/api"
+// export const BASE_URL = "https://dummy-backend-final-production.up.railway.app/api"
+export const BASE_URL = "https://dummydoserver.dostudio.info/api"
 // export const BASE_URL = "http://localhost:2024/api"
